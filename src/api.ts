e21e87041d911aa401f972032c4ff21542552393
shyamalamrgn <shyamalamrgn@gmail.com>
2025-12-12 08:38:55 +0530

    made changes to api

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 // src/api.ts
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:10000";
 
 export function apiUrl(path: string) {
   if (!path.startsWith("/")) path = "/" + path;
